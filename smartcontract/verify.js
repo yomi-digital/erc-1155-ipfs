94cb165c66d7c5ee194f892253f5da3c75b9e7a2
turinglabsorg <sebastiano.cataudo@gmail.com>
2022-01-18 13:43:44 +0100

    fixing readme

--- smartcontract/verify.js
+++ smartcontract/verify.js
@@ -5,15 +5,18 @@ const child_process = require('child_process')
 async function deploy() {
     try {
         const configs = JSON.parse(fs.readFileSync('./configs/' + argv._ + '.json').toString())
+
         if (
             configs.network !== undefined &&
+            configs.owner_mnemonic !== undefined &&
             configs.owner_address !== undefined &&
-            configs.owner_mnemonic !== undefined
+            configs.provider !== undefined &&
+            configs.contract_name !== undefined &&
+            configs.polygonscan_key !== undefined
         ) {
 
-
             console.log('Verifying contract..')
-            child_process.execSync('sudo PROVIDER="' + configs.provider + '" MNEMONIC="' + configs.owner_mnemonic + '" truffle run verify Erc1155IPFS --network ' + configs.network, { stdio: 'inherit' })
+            child_process.execSync('sudo PROVIDER="' + configs.provider + '" MNEMONIC="' + configs.owner_mnemonic + '" POLYGONSCAN_KEY="' + configs.polygonscan_key + '" OWNER="' + configs.owner_address + '" truffle run verify ' + configs.contract_name + ' --network ' + configs.network + ' --reset', { stdio: 'inherit' })
 
             console.log('All done, exiting!')
             process.exit();
